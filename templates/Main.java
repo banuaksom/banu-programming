a0859a432d411f8df561308ecee75710c85acbf7
Banu <banu@Sergeys-MacBook-Pro.local>
2017-11-30 22:27:56 -0800

    change line

--- templates/Main.java
+++ templates/Main.java
@@ -1,6 +1,6 @@
 // Simple hello world template
 public class Main {
       public static void main(String [] args) {
-        System.out.println("Hello world!");
+        System.out.println("I love you!");
     }
 }
